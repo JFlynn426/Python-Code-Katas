8ba87112f115c812dcf6bb9b1fc5475e54727aff
Jflynn426 <jflynnpics@gmail.com>
2019-09-22 18:13:44 -0400

    first python code kata

--- Python Code Katas/Python_Code_Katas.py
+++ Python Code Katas/Python_Code_Katas.py
@@ -1,1 +1,9 @@
-
+def Descending_Order(num):
+    list = []
+    string = ""
+    for number in str(num):
+        list.append(number)
+    list.sort()
+    for char in range (len(list)-1, -1, -1):
+        string = string + list[char]
+    return int(string)

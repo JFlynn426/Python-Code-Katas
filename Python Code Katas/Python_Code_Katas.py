05570c8fc4c378ee68bf87a540fbbaf4232ac624
Jflynn426 <jflynnpics@gmail.com>
2019-09-23 15:48:59 -0400

    odd number pyramid

--- Python Code Katas/Python_Code_Katas.py
+++ Python Code Katas/Python_Code_Katas.py
@@ -7,3 +7,11 @@ def Descending_Order(num):
     for char in range (len(list)-1, -1, -1):
         string = string + list[char]
     return int(string)
+def row_sum_odd_numbers(n):
+    x = 1
+    y = 0
+    for i in range (1, n):
+        x += 2 * i
+    for j in range (1, n):
+        y += 2 * j
+    return (n * x + y)

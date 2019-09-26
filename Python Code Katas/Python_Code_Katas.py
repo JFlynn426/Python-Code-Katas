d3b50a423939155d071c33622f5ef05dd4df4420
Jflynn426 <jflynnpics@gmail.com>
2019-09-25 20:49:53 -0400

    duplicate count

--- Python Code Katas/Python_Code_Katas.py
+++ Python Code Katas/Python_Code_Katas.py
@@ -28,3 +28,18 @@ def is_square(n):
         return True
     else:
         return False
+
+def duplicate_count(text):
+    text = text.lower()
+    SortedString = list(text)
+    SortedString.sort()
+    count = 0
+    i = 0
+    while i < len(SortedString) - 1:
+        if SortedString[i] == SortedString[i + 1]:
+            count += 1
+            while SortedString[i] == SortedString[i + 1] and i < len(SortedString) - 2:
+                i += 1
+        else:
+            i += 1
+    return count
